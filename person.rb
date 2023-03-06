afe48562d600ef6e7f480a870f22f73f70e3c6e8
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2023-03-06 13:14:02 +0200

    Fix rubocop offences and Rename is_of_age? to of_age?

--- person.rb
+++ person.rb
@@ -1,5 +1,5 @@
 class Person
-  def initialize(age, name: "Unknown", parent_permission: true)
+  def initialize(age, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -10,9 +10,10 @@ class Person
   attr_accessor :name, :age
 
   private
-  
-  def is_of_age?
+
+  def of_age?
     return true if @age >= 18
+
     false
   end
 
@@ -21,5 +22,4 @@ class Person
   def can_use_services?
     is_of_age? || @parent_permission
   end
-
 end
--- student.rb
+++ student.rb
@@ -1,13 +1,12 @@
-require_relative './person.rb'
+require_relative './person'
 
 class Student < person
-  def initialize(age, classroom, name: "Unknown", parent_permission: true)
+  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
-    "¯\(ツ)/¯"
+    '¯(ツ)/¯'
   end
-
 end
