ecb4d58cebeb11e791bed308a14ca40bb942b84c
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2023-03-06 14:04:56 +0200

    Use a capital letter for the P since it is a class I am referencing

--- student.rb
+++ student.rb
@@ -1,6 +1,6 @@
 require_relative './person'
 
-class Student < person
+class Student < Person
   def initialize(age, classroom, name: 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
