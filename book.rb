0935404710cd874939736956c4bcc9c806faa97c
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2023-03-08 11:09:44 +0200

    Add Book,Rental class and Create many-to-many relationship between Person, Book classes using Rental class

---
+++ book.rb
@@ -0,0 +1,14 @@
+class Book
+  attr_accessor :title, :author, :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def rented(rental)
+    @rentals << rental
+  end
+end
+
